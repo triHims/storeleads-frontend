bebe8dafeb914070e52e49261d9be0fc41f3a679
trihims <himanshu.16bcs1156@abes.ac.in>
2023-02-16 12:17:59 +0530

    remove verification url and use fetch to do all the work

--- src/components/servicecalls/storeleadsapi.js
+++ src/components/servicecalls/storeleadsapi.js
@@ -1,17 +1,6 @@
 import axios from "axios";
 
 const BASEURL = "http://localhost:8000/"
-export async function verifyStoreLeadsUrl(storeLeadsUrl) {
-    let response
-    try {
-        response = await axios.get(BASEURL + "storeleadsUtl/verifyStoreLeadsUrl", { params: { storeleadsURL: storeLeadsUrl } })
-    } catch (error) {
-        console.error(error)
-        response = {}
-    }
-    return response
-
-}
 
 export async function fetchDataFromStoreLeadsUrl(storeLeadsUrl,pageSize) {
     let response
